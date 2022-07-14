c2030ffc79b331d45c3370e8cb6fb104a5719882
hamza <hamza.bakouchi@gmail.com>
2022-07-14 19:22:28 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  resolve: {
+    alias: {
+      '@r': path.resolve('resources'),
+      '@': path.resolve('resources/js'),
+    },
+  },
+  // plugins: [
+  //   new webpack.DefinePlugin({
+  //     __VUE_OPTIONS_API__: true,
+  //     __VUE_PROD_DEVTOOLS__: true,
+  //   }),
+  // ],
+};
