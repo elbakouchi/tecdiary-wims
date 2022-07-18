23a82add3394e91eb7fcc7df1996b73287fa775b
hamza <hamza.bakouchi@gmail.com>
2022-07-18 02:13:20 +0100

    limit fields to bare necessary

--- app/Http/Resources/ContactAutoComplete.php
+++ app/Http/Resources/ContactAutoComplete.php
@@ -14,6 +14,9 @@ class ContactAutoComplete extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'value' => $this->id,
+            'label' => $this->name,
+        ];
     }
 }
