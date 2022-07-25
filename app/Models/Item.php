fce66b37adaef5f90c11aa3d032c59a50bb25bb9
hamza <hamza.bakouchi@gmail.com>
2022-07-25 11:23:56 +0100

    exception trace

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -77,7 +77,7 @@ class Item extends Model
 
     public function scopeFilter(Builder $builder, $request)
     {
-        return (new CheckinItemFilter($request->contact, $request->warehouse))->apply($builder);
+        return (new CheckinItemFilter($request->only('contact'), $request->only('warehouse')))->apply($builder);
     }
 
     public function scopeFromCategory($query, $category)
