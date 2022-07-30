18450b1d6f2531f3227e4235f1e78cad4e180eb2
hamza <hamza.bakouchi@gmail.com>
2022-07-30 20:39:13 +0100

    comment details

--- app/Filters/Models/ItemSearchFitler.php
+++ app/Filters/Models/ItemSearchFitler.php
@@ -18,7 +18,7 @@ class ItemSearchFitler extends AbstractEloquentFilter
     {
         return $query->where('name', 'like', "%$this->search%")
                    ->orWhere('sku', 'like', "%$this->search%")
-                   ->orWhere('details', 'like', "%$this->search%")
+                  // ->orWhere('details', 'like', "%$this->search%")
                    ->orWhere('symbology', 'like', "%$this->search%")
                    ->orWhere('rack_location', 'like', "%$this->search%")
                    //->orWhere('items.account_id', '=', 1)
