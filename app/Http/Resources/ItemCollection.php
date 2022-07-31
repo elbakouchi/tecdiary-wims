47c449d275c63b85b4c1c214b3a3d3f1abb8c073
hamza <hamza.bakouchi@gmail.com>
2022-07-31 19:02:55 +0100

    fix wrap

--- app/Http/Resources/ItemCollection.php
+++ app/Http/Resources/ItemCollection.php
@@ -6,14 +6,14 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class ItemCollection extends ResourceCollection
 {
-    public static $wrap = 'item';
+    public static $wrap = 'data';
 
     public function toArray($request)
     {
         //dd($request);
         //return parent::toArray($request);
         return $this->collection->transform(function($item){
-            //dd($item);
+            //dd($item)
             return [
                 'id'               => $item->id,
                 'sku'              => $item->sku,
@@ -32,13 +32,13 @@ class ItemCollection extends ResourceCollection
                 'has_variants'     => $item->has_variants == 1 ? true : false,
                 'track_weight'     => $item->track_weight == 1 ? true : false,
                 'track_quantity'   => $item->track_quantity == 1 ? true : false,
-                //'user'             => $item->whenLoaded('user'),
-                //'unit'             => $item->whenLoaded('unit'),
-                //'stock'            => $item->whenLoaded('stock'),
-                //'serials'          => $item->whenLoaded('serials'),
-                //'all_stock'        => $item->whenLoaded('allStock'),
-                //'categories'       => $item->whenLoaded('categories'),
-                //'variations'       => $item->whenLoaded('variations'),
+                'user'             => $item->whenLoaded('user'),
+                'unit'             => $item->whenLoaded('unit'),
+                'stock'            => $item->whenLoaded('stock'),
+                'serials'          => $item->whenLoaded('serials'),
+                'all_stock'        => $item->whenLoaded('allStock'),
+                'categories'       => $item->whenLoaded('categories'),
+                'variations'       => $item->whenLoaded('variations'),
             ];
         });
     }
