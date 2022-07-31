c711055860a7afe727a009b81dafa352ffa4bfd4
hamza <hamza.bakouchi@gmail.com>
2022-07-31 18:55:25 +0100

    debug

--- app/Http/Resources/ItemCollection.php
+++ app/Http/Resources/ItemCollection.php
@@ -13,7 +13,7 @@ class ItemCollection extends ResourceCollection
         //dd($request);
         //return parent::toArray($request);
         return $this->collection->transform(function($item){
-            dd($item);
+            //dd($item);
             return [
                 'id'               => $item->id,
                 'sku'              => $item->sku,
