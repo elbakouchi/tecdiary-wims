2b9b5e904c3bab3edb9ece93be829b8d56d1fd2f
hamza <hamza.bakouchi@gmail.com>
2022-08-01 07:10:30 +0100

    warehouse fields

--- app/Http/Resources/WarehouseResource.php
+++ app/Http/Resources/WarehouseResource.php
@@ -11,6 +11,14 @@ class WarehouseResource extends JsonResource
         return [
             'value' => $this->id,
             'label' => $this->name,
+            'id' => $this->id,
+            'name' => $this->name,
+            'code' => $this->code,
+            'deleted_at' => $this->deleted_at,
+            'phone' => $this->phone,
+            'email'=> $this->email,
+            'address' => $this->address,
+            'active'=> $this->active
         ];
     }
 }
